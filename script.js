91c90febde51da9a146b527c297e4726f5cee617
galaxy233 <vjmatonis@gmail.com>
2017-06-25 19:24:57 -0600

    connect to api

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 var starshipMap = {
   'CR90 Corvette' : {
-    'id' : 2,
-    'imgPath' : "img/CR90.jpg",
-    'data' : {
-      'cost' : 1000,
-      'speed' : 1000,
-      'cargo' : 1000,
-      'passengers' : 1000
-    }
+    id : 2,
+    imgPath : "img/CR90.jpg"
   },
   'V-Wing' : {
-    'id' : 75,
-    'imgPath' : "img/Vwing.jpg",
-    'data' : {
-      'cost' : 9000,
-      'speed' : 9000,
-      'cargo' : 9000,
-      'passengers' : 9000
-    }
+    id : 75,
+    imgPath : "img/Vwing.jpg"
   },
   'Belbullab-22 Starfighter' : {
-    'id' : 74,
-    'imgPath' : "img/Belbullab-22.jpg",
-    'data' : {
-      'cost' : 8000,
-      'speed' : 8000,
-      'cargo' : 8000,
-      'passengers' : 8000
-    }
+    id : 74,
+    imgPath : "img/Belbullab-22.jpg"
+  },
+  'Jedi Interceptor': {
+    id : 65,
+    imgPath : "img/Interceptor.jpg"
+  },
+  'Star Destroyer': {
+    id : 3,
+    imgPath : "img/Star-Destroyer.jpg"
+  },
+  'Trade Fedaration Cruiser': {
+    id : 59,
+    imgPath : "img/Trade-Federation.jpg"
+  },
+  'Solar Sailer': {
+    id : 58,
+    imgPath : "img/Solar_sail.jpg"
+  },
+  'Republic Attack Cruiser': {
+    id : 63,
+    imgPath : "img/Republic_Attack.jpeg"
+  },
+  'A-wing': {
+    id : 28,
+    imgPath : "img/AWing.png"
+  },
+  'B-Wing': {
+    id : 29,
+    imgPath : "img/BWing.jpg"
+  },
+  'Naboo Fighter': {
+    id : 39,
+    imgPath : "img/Naboo.jpg"
+  },
+  'Millenium Falcon': {
+    id : 10,
+    imgPath : "img/millennium.jpg"
   }
 }
 
@@ -65,11 +83,36 @@ function updateCells(cells, data) {
 }
 
 function initializeView() {
-  updateView('left', leftIdx);
-  updateView('right', rightIdx);
+  var left = getStarshipPromise(leftIdx);
+  var right = getStarshipPromise(rightIdx);
+  left.then(data => updateView('left', leftIdx, data))
+  right.then(data => updateView('right', rightIdx, data))
 }
 
-function updateView(view, index) {
+function getStarshipPromise(index) {
+  var starship = starshipArr[index];
+  var promise = new Promise(function(resolve, reject) {
+    if (!starshipMap[starship].data) {
+      fetch("http://swapi.co/api/starships/" + starshipMap[starship].id).then(function(resp) {
+        return resp.json();
+      }).then(function(data) {
+        var starshipData = {
+          cost:data["cost_in_credits"],
+          speed:data["max_atmosphering_speed"],
+          cargo:data["cargo_capacity"],
+          passengers:data["passengers"]
+        }
+        starshipMap[starship].data = starshipData;
+        resolve(starshipData);
+      })
+    } else {
+      resolve(starshipMap[starship].data)
+    }
+  })
+  return promise;
+}
+
+function updateView(view, index, data) {
   // Update browser with data for starship at index i in starshipArr. Assumes i is valid
   // index in starshipArr.
   if (view === 'left') {
@@ -85,54 +128,43 @@ function updateView(view, index) {
   var speed = accessTableCell(table, 3);
   var cargo = accessTableCell(table, 5);
   var passengers = accessTableCell(table, 7);
-  var data = getDataFromCache(index);
   name.innerHTML = starshipArr[index];
   img.src = starshipMap[starshipArr[index]]['imgPath'];
   updateCells([cost, speed, cargo, passengers], data);
 }
 
-function getDataFromCache(index) {
-  // Checks if data already exists in cache. If not, the data is requested from the
-  // server and stored in the cache.
-  var starship = starshipArr[index];
-  if (starshipMap[starship].data) {
-    return starshipMap[starship].data;
-  } else {
-    starshipMap[starship].data = getDataFromServer(index);
-    return starshipMap[starship].data;
-  }
-}
-
-function getDataFromServer(index) {
-  // async code goes here
-}
 // Add event listeners with jQuery
 $("#starship-left-toggle-left").click(function() {
   leftIdx = prevIdx(leftIdx);
   if (leftIdx === rightIdx) {
     leftIdx = prevIdx(leftIdx);
   }
-  updateView('left', leftIdx);
+  var promise = getStarshipPromise(leftIdx);
+  promise.then(data => updateView('left', leftIdx, data));
 })
 $("#starship-left-toggle-right").click(function() {
   leftIdx = nextIdx(leftIdx);
   if (leftIdx === rightIdx) {
     leftIdx = nextIdx(leftIdx);
   }
-  updateView('left', leftIdx);
+  var promise = getStarshipPromise(leftIdx);
+  promise.then(data => updateView('left', leftIdx, data));
 })
 $("#starship-right-toggle-left").click(function() {
   rightIdx = prevIdx(rightIdx);
   if (rightIdx === leftIdx) {
     rightIdx = prevIdx(rightIdx);
   }
-  updateView('right', rightIdx);
+  var promise = getStarshipPromise(rightIdx);
+  promise.then(data => updateView('right', rightIdx, data));
 })
 $("#starship-right-toggle-right").click(function() {
   rightIdx = nextIdx(rightIdx);
   if (rightIdx === leftIdx) {
     rightIdx = nextIdx(rightIdx);
   }
-  updateView('right', rightIdx);
+  var promise = getStarshipPromise(rightIdx);
+  promise.then(data => updateView('right', rightIdx, data));
 })
+
 initializeView();
